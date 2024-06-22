b88d790ba4cef9435e28ce068a65711e8cc15710
Jose Solorzano <jose.h.solorzano@gmail.com>
2024-06-22 08:38:20 -0500

    Simpler model

--- audio_denoiser/modules/LightweightDenoiser.py
+++ audio_denoiser/modules/LightweightDenoiser.py
@@ -29,22 +29,19 @@ class LightweightDenoiser(nn.Module, PyTorchModelHubMixin):
         bias_param = torch.zeros((1, self.frame_size))
         self.bias = nn.Parameter(bias_param)
 
-    def forward(self, frame: torch.Tensor):
-        # frame: (batch_size, frame_size)
-        frame_fft = torch.fft.fft(frame, dim=1)
-        frame_fft_abs = torch.abs(frame_fft)
+    def forward(self, frame_fft: torch.Tensor):
+        # frame_fft: (batch_size, 2, frame_size)
+        # First channel is real, second is complex
+        frame_fft_abs = torch.sqrt(torch.sum(frame_fft.pow(2), dim=1, keepdim=True))
         frame_fft_abs = (frame_fft_abs - self.abs_fft_mean) / self.abs_fft_scale
-        frame_fft_abs = frame_fft_abs[:, None, :]
         preserve_f = (self.model(frame_fft_abs) + self.bias) / 2 + 0.5
         preserve_f = torch.clamp(preserve_f, min=0, max=1)
-        frame_fft = frame_fft * preserve_f
-        out_frame = torch.fft.ifft(frame_fft).real
-        return out_frame
+        return preserve_f.squeeze(1)
 
 
 if __name__ == '__main__':
     _config = LightweightDenoiserConfig()
-    x = torch.randn((500, _config.frame_size))
+    x = torch.randn((500, 2, _config.frame_size))
     _m = LightweightDenoiser(_config)
     _y = _m(x)
     print(_y.shape)
